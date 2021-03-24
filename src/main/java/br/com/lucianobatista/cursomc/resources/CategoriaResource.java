55bb962c6be71d8f4ab62332fad3f9c39a584d0c
Luciano Batista <lucianobatista01@yahoo.com.br>
2021-03-24 14:13:39 -0300

    Testando a primeira classe de dominio - Caregoria

--- src/main/java/br/com/lucianobatista/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/lucianobatista/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.lucianobatista.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.lucianobatista.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escrotório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
