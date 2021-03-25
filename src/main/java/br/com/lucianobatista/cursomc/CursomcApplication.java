e4501772fdd2ec4f74e5cffc692cf757242a4977
Luciano Batista <lucianobatista01@yahoo.com.br>
2021-03-24 21:11:54 -0300

    Estado e Cidade

--- src/main/java/br/com/lucianobatista/cursomc/CursomcApplication.java
+++ src/main/java/br/com/lucianobatista/cursomc/CursomcApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 import br.com.lucianobatista.cursomc.domain.Categoria;
+import br.com.lucianobatista.cursomc.domain.Cidade;
+import br.com.lucianobatista.cursomc.domain.Estado;
 import br.com.lucianobatista.cursomc.domain.Produto;
 import br.com.lucianobatista.cursomc.repository.CategoriaRepository;
+import br.com.lucianobatista.cursomc.repository.CidadeRepository;
+import br.com.lucianobatista.cursomc.repository.EstadoRepository;
 import br.com.lucianobatista.cursomc.repository.ProdutoRepository;
 
 @SpringBootApplication
@@ -23,6 +25,13 @@ public class CursomcApplication implements CommandLineRunner{
 	@Autowired
 	ProdutoRepository produtoRepository;
 
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -46,6 +55,20 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+	
 		
 	}
 
